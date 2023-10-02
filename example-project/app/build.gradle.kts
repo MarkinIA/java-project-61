f5bc93796658d7df481fbf88dce2359ef8e76b2e
Markin Ilya <markin.il86@gmail.com>
2023-10-02 16:38:09 +0300

    bug fix

--- example-project/app/build.gradle.kts
+++ example-project/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
-    id("se.patrikerdes.use-latest-versions version '0.2.18'")
-    id("com.github.ben-manes.versions' version '0.41.0'")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 group = "hexlet.code"
