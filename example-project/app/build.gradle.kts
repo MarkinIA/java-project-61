33c83b5584a17506fd4de94d2099ce7f3208bcbf
Markin Ilya <markin.il86@gmail.com>
2023-10-02 17:14:37 +0300

    changed output text

--- example-project/app/build.gradle.kts
+++ example-project/app/build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
+    application
     id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.47.0"
 }
+application {mainClass.set("hexlet.code.App")}
+
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
